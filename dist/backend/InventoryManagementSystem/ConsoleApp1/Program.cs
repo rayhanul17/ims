96e001f2c5abcacb7845e8659e73b004a34921ff
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-06-25 18:03:10 +0600

    add ninject

--- dist/backend/InventoryManagementSystem/ConsoleApp1/Program.cs
+++ dist/backend/InventoryManagementSystem/ConsoleApp1/Program.cs
@@ -5,12 +5,12 @@ using (var session = NHibernateHelper.GetSession())
     using (var tx = session.BeginTransaction())
     {
 
-        var product = new Product { Name = "zzzz", Price = 120.65m };
-        var category = new Category { Name = "Category Zzzz", Products = new List<Product> { product } };
-        product.Category = category;
+        var product = new Product { Name = "Rajaaaaaaaaaaaa", Price = 120.65m };
+        var category = new Category { Name = "Category User02", Products = new List<Product> { product } };
+        //product.Category = category;
         
 
-        session.Save(product);
+        session.Save(category);
         tx.Commit();
 
         //var c = session.Get<Category>(5);
