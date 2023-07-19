2964e47b4bbdba90545efe75190c20aef30cdbbc
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-07-19 18:40:48 +0600

    Theme menu update

---
+++ src/IMS/web/IMS/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult LP()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
