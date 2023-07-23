bbca246e780c8f55a1a2e5926e74a75c226d87de
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-07-23 13:05:32 +0600

    dynamically row add

---
+++ src/IMS/web/IMS/Controllers/PurchaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMS.Controllers
+{
+    public class PurchaseController : AllBaseController
+    {
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            return RedirectToAction("Create");
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
