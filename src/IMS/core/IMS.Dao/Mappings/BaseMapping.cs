102cd9b5ffa5674178e7ca20d0d4eb6e0d967e16
Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2023-08-13 16:51:13 +0600

    configure version number

--- src/IMS/core/IMS.Dao/Mappings/BaseMapping.cs
+++ src/IMS/core/IMS.Dao/Mappings/BaseMapping.cs
@@ -9,14 +9,15 @@ namespace IMS.Dao.Mappings
         public BaseMapping()
         {
             Id(x => x.Id).Not.Nullable();
+            Map(x => x.Status).Not.Nullable();
+            Map(x => x.Rank).Nullable();
             Map(x => x.CreateBy).Not.Nullable();
             Map(x => x.CreationDate).Not.Nullable();
             Map(x => x.ModifyBy).Nullable();
             Map(x => x.ModificationDate).Nullable();
-            Map(x => x.Status).Not.Nullable();
-            Map(x => x.Rank).Nullable();
-            Map(x => x.VersionNumber).Nullable();
             Map(x => x.BusinessId).Nullable();
+            Version(x => x.VersionNumber).Not.Nullable();
+
         }
     }
 }
